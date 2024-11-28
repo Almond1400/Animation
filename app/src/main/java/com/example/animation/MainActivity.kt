b131e9a4ba3f65bf5436939f6bc9dedc91d70d65
Almond1400 <mpcknna@gmail.com>
2024-11-28 16:24:43 +0900

    Initial commit

---
+++ app/src/main/java/com/example/animation/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.animation
+
+import android.content.res.Configuration
+import android.graphics.drawable.AnimationDrawable
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView.Orientation
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        val img:ImageView = findViewById(R.id.ivAnim)
+
+        img.setBackgroundResource(R.drawable.animation);
+        val frameAnimation = img.background as AnimationDrawable
+        frameAnimation.start()
+    }
+}
